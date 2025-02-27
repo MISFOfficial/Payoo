a4368b7079c85cb72c84bc0dc7b4dc37aab40fd4
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-02-27 20:14:04 +0600

    login page complete

---
+++ script.js
@@ -0,0 +1,21 @@
+// Login function
+document.getElementById('btn-login').addEventListener('click', function(){
+    const emails = document.getElementById('number')
+    const inputEmail=emails.value;
+    if(inputEmail.length==11){
+        const pins=document.getElementById('pin')
+        const inputPin=pins.value;
+        if(inputPin==='1234'){
+            window.location.href="home.html";
+        }
+        else{
+            alert('Pin is 1234')
+        }
+    }
+    else{
+        alert("User ID is invalid, type 11 Digit number")
+    }  
+})
+
+
+// Home Function
